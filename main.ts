b2c5c70387207a133b77120c0c278fbb07e584c2
Nil Karadag <nil.karadag@holition.com>
2021-05-16 23:04:44 +0100

    reactor: text change in main

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 
 import { CLI } from "./src/cli/cli.module";
 
-console.log('Welcome to Coin Change Parser!');
+console.log('Welcome to Coin Change CLI!');
 const cli = new CLI();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (inputBuffer: string) => {
